1bf5e6bac1f2ef91b8618f71b54c6fdfd8018c71
web <110479064+personalizeweb@users.noreply.github.com>
2022-08-04 18:53:41 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//maps
+function initMap() {
+
+  var macc = {lat: 42.1382114, lng: -71.5212585};
+
+  var map = new google.maps.Map(
+
+      document.getElementById('map'), {zoom: 15, center: macc});
+
+  var marker = new google.maps.Marker({position: macc, map: map});
+
+}
+
